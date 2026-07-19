8f18a69b2d3a0f71d9b1abdd9300990848b2fca5
Soad-Alzain <soadalzain640@gmail.com>
2026-07-19 08:48:02 +0300

    Add files via upload

---
+++ src/components/contactAlert.vue
@@ -0,0 +1,11 @@
+<template>
+     <div class="alert  mt-5 shadow" style="width:50%;margin:auto;background:white;color:#FF725E;" v-show="$store.state.shownItem=='alert'">
+      <h4><span class="bi bi-check-circle rounded-pill text-light" style="color:#FF725E !important; "></span><b> Thank you</b> for contacting us!</h4>
+    </div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+<style></style>
